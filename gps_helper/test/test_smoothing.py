742287573062a21cbe7f6a8cd3d991bcc3272bbc
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2018-05-13 18:47:40 -0600

    Adding test for average filter

---
+++ gps_helper/test/test_smoothing.py
@@ -0,0 +1,11 @@
+from .. import smoothing
+from unittest import TestCase
+
+class TestSmoothing(TestCase):
+
+    def test_average_filter(self):
+        af = smoothing.AverageFilter()
+        x_vector = [10, 20, 30]
+        for xk in x_vector:
+            af.next_sample(xk)
+        self.assertEqual(af.x_k, 20)
