18b52319b2c733008a249e2778d52e7b66b63b56
Chiranth Siddappa <chiranth.siddappa@cosmicaes.com>
2018-06-12 01:08:58 -0600

    Adding low pass filter test

--- gps_helper/test/test_smoothing.py
+++ gps_helper/test/test_smoothing.py
@@ -52,3 +52,16 @@ class TestSmoothing(GPSTest):
         for k, z_k in enumerate(z):
             y20[k] = ma.next_sample(z_k)
         npt.assert_almost_equal(y20[-5:], y_test)
+
+    def test_low_pass_filter(self):
+        y_test = np.array([2.89877566, 3.03393351, 3.15150167, 3.28423681, 3.45673418])
+        lf = sm.LowPassFilter(0.9)
+        Npts = 100
+        n = np.arange(0, 100)
+        x = 5 * np.cos(2 * np.pi * n / 100)
+        v = 0.3 * np.random.randn(Npts)
+        z = x + v
+        ylf = np.zeros_like(z)
+        for k, z_k in enumerate(z):
+            ylf[k] = lf.next_sample(z_k)
+        npt.assert_almost_equal(ylf[-5:], y_test)
