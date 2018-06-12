e7fa9fef8a4bf823a3877b66e0df0fedbd011965
Chiranth Siddappa <chiranth.siddappa@cosmicaes.com>
2018-06-12 02:39:13 -0600

    Fixing paths for source code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ import os
 # import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-sys.path.insert(0, os.path.abspath('../../gps_helper'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- General configuration ------------------------------------------------
