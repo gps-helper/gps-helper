bc7a852b011d1e867d8a29d252e0fc1f602c78cc
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2018-06-24 01:06:05 -0600

    Adding nbsphinx to convert jupyter notebooks into a format for sphinx and rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode',
+              'nbsphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
