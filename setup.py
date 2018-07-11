bebd12526bf52fcb09555a8f3a918626f34e1241
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2018-07-10 23:25:51 -0500

    Ready for release 0.0.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='gps_helper',
-      version='0.0.4',
+      version='0.0.5',
       description='GPS helper module',
       long_description=long_description,
       long_description_content_type="text/markdown",
