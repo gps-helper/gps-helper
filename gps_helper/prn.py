1ddabfa22c29bcc45714ba40eb2397313973bb47
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2018-08-07 04:35:44 -0600

    Adding G polynomial taps to json file; ShiftRegister class added

--- gps_helper/prn.py
+++ gps_helper/prn.py
@@ -3,3 +3,16 @@ import os
 
 prn_json = open(os.path.join(os.path.dirname(__file__), 'prn_info.json')).read()
 prn_info = json.loads(prn_json)
+
+
+class ShiftRegister:
+
+    def __init__(self, G, poly_taps, prn_taps):
+        """
+
+        :param poly_taps: The polynomial taps to be used for the shift register
+        :param prn_taps: The taps for the output
+        """
+        self.G = G
+        self.poly_taps = poly_taps
+        self.prn_taps = prn_taps
