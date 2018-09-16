0e06f0462b479f09e1b07db235b51cc71cb53e8f
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2018-09-16 16:12:37 -0600

    Adding tests for the g1 and g2 polynomials.

--- gps_helper/test/test_prn.py
+++ gps_helper/test/test_prn.py
@@ -7,3 +7,27 @@ class TestPRN(GPSTest):
 
     def test_prn_info(self):
         prn.prn_info
+
+    def test_prn_G1(self):
+        """
+        Test the first ten bits of output from G1
+        :return:
+        """
+        g1_test = [0, 0, 0, 1, 1, 1, 1, 1, 1, 1]
+        g1_poly = prn.prn_info['taps']['G1']
+        sr = prn.ShiftRegister(g1_poly, [10])
+        for i in range(3):
+            sr.next()
+        self.assertEqual(g1_test, list(sr.G))
+
+    def test_prn_G2(self):
+        """
+        Test the first ten bits of output from G2 using prn 1
+        :return:
+        """
+        g2_test = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        g2_poly = prn.prn_info['taps']['G2']
+        prn1_poly = prn.prn_info['taps']['1']
+        sr = prn.ShiftRegister(g2_poly, prn1_poly)
+        sr.next()
+        self.assertEqual(g2_test, list(sr.G))
